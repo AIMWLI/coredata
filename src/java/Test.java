6b629caaab23bf754fae18267302cad94ae03948
songjin <songjin@detadata.com>
2020-04-01 01:06:11 +0800

    init master, coding on the develop

--- src/java/Test.java
+++ src/java/Test.java
@@ -3,6 +3,7 @@ import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
+        //查看hashMap底层实现
         Map<String, Object> map = new HashMap<>();
         map.put("1", 1);
         map.put("2", 2);
