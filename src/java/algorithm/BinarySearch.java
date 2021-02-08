05fdd2959055914fe5e1ea712ef056e8696dec2a
AIMWLI <jin17.song@gmail.com>
2021-02-08 11:20:00 +0800

    binary tree inorder recursive, binary search recursive

--- src/java/algorithm/BinarySearch.java
+++ src/java/algorithm/BinarySearch.java
@@ -12,4 +12,16 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    public static int searchRecursive(int[] arr, int target) {
+        return binary(arr, 0, arr.length - 1, target);
+    }
+
+    private static int binary(int[] arr, int left, int right, int target) {
+        if (left > right) return -1;
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return mid;
+        if (arr[mid] < target) return binary(arr, mid + 1, right, target);
+        return binary(arr, left, mid - 1, target);
+    }
 }
--- src/java/leetcode/BinaryTreeInorder.java
+++ src/java/leetcode/BinaryTreeInorder.java
@@ -27,4 +27,17 @@ public class BinaryTreeInorder {
         }
         return res;
     }
+
+    public List<Integer> inorderRecursive(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(TreeNode node, List<Integer> res) {
+        if (node == null) return;
+        dfs(node.left, res);
+        res.add(node.val);
+        dfs(node.right, res);
+    }
 }
