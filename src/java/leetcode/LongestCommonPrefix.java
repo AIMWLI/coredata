8c018db4c8005e0e20fedf41bbaaf210d78ceab6
AIMWLI <jin17.song@gmail.com>
2021-02-15 14:05:00 +0800

    longest common prefix with sort, zigzag level order

--- src/java/leetcode/LongestCommonPrefix.java
+++ src/java/leetcode/LongestCommonPrefix.java
@@ -13,4 +13,13 @@ public class LongestCommonPrefix {
         }
         return prefix;
     }
+
+    public String longestCommonPrefixSort(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        java.util.Arrays.sort(strs);
+        String first = strs[0], last = strs[strs.length - 1];
+        int i = 0;
+        while (i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)) i++;
+        return first.substring(0, i);
+    }
 }
