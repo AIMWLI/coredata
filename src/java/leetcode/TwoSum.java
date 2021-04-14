23edc2a743e10c0ae681c1399bdc8d4d474fb0d9
AIMWLI <jin17.song@gmail.com>
2021-04-14 09:50:00 +0800

    two sum sorted two pointer

--- src/java/leetcode/TwoSum.java
+++ src/java/leetcode/TwoSum.java
@@ -16,6 +16,18 @@ public class TwoSum {
         return new int[]{-1, -1};
     }
 
+    public int[] twoSumSorted(int[] nums, int target) {
+        java.util.Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) return new int[]{i, j};
+            if (sum < target) i++;
+            else j--;
+        }
+        return new int[]{-1, -1};
+    }
+
     public int[] twoSumBrute(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
