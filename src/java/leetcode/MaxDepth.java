db4b9f946c0cd0f04f1ed524a0d18acb7ee1ce55
AIMWLI <jin17.song@gmail.com>
2021-07-07 10:40:00 +0800

    climb stairs dp, max depth binary tree

---
+++ src/java/leetcode/MaxDepth.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class MaxDepth {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
