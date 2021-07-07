3bab334958885ce216b116cc1ae33ba66d276430
AIMWLI <jin17.song@gmail.com>
2021-07-07 15:50:00 +0800

    merge sort count inversions

--- src/java/algorithm/MergeSort.java
+++ src/java/algorithm/MergeSort.java
@@ -30,4 +30,23 @@ public class MergeSort {
         int[] tmp = new int[arr.length];
         mergeSort(arr, tmp, left, right);
     }
+
+    public static int inversions(int[] arr) {
+        int[] tmp = new int[arr.length];
+        return countInv(arr, tmp, 0, arr.length - 1);
+    }
+
+    private static int countInv(int[] arr, int[] tmp, int left, int right) {
+        if (left >= right) return 0;
+        int mid = left + (right - left) / 2;
+        int inv = countInv(arr, tmp, left, mid) + countInv(arr, tmp, mid + 1, right);
+        System.arraycopy(arr, left, tmp, left, right - left + 1);
+        int i = left, j = mid + 1, k = left;
+        while (i <= mid && j <= right) {
+            if (tmp[i] <= tmp[j]) arr[k++] = tmp[i++];
+            else { arr[k++] = tmp[j++]; inv += mid - i + 1; }
+        }
+        while (i <= mid) arr[k++] = tmp[i++];
+        return inv;
+    }
 }
