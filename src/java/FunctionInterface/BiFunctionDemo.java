b21d6c1cecc0010251103e207bd9e6bd078a2f8f
AIMWLI <jin17.song@gmail.com>
2025-08-05 09:45:00 +0800

    bifunction binaryOperator

--- src/java/FunctionInterface/BiFunctionDemo.java
+++ src/java/FunctionInterface/BiFunctionDemo.java
@@ -1,6 +1,7 @@
 package FunctionInterface;
 
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 
 public class BiFunctionDemo {
 
@@ -10,5 +11,11 @@ public class BiFunctionDemo {
 
         BiFunction<Integer, Integer, String> toString = (a, b) -> a + " + " + b;
         System.out.println("toString: " + toString.apply(3, 5));
+
+        BiFunction<Integer, Integer, Integer> max = (a, b) -> a > b ? a : b;
+        System.out.println("max: " + max.apply(7, 3));
+
+        BinaryOperator<Integer> sum = (a, b) -> a + b;
+        System.out.println("binaryOperator: " + sum.apply(10, 20));
     }
 }
