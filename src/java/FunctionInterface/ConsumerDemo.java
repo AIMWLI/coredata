390f8490a3b1e8d2a74dd83d48dc82e304d9be74
AIMWLI <jin17.song@gmail.com>
2025-08-15 15:10:00 +0800

    consumer wrapped

--- src/java/FunctionInterface/ConsumerDemo.java
+++ src/java/FunctionInterface/ConsumerDemo.java
@@ -40,6 +40,10 @@ public class ConsumerDemo {
         };
         safePrint.accept("not null");
 
+        Consumer<String> wrapWithBrackets = s -> System.out.println("[" + s + "]");
+        Consumer<String> log = s -> System.out.println("log: " + s);
+        wrapWithBrackets.andThen(log).accept("wrapped");
+
         while (true) {
             Consumer<String> consumer = System.out::println;
             Scanner scanner = new Scanner(System.in);
