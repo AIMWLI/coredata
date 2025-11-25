11946884e893c547eb6fa49f8cc755e986ebe3de
AIMWLI <jin17.song@gmail.com>
2025-11-25 10:05:00 +0800

    stringBuilder append demo

---
+++ src/java/lang/StringBuilderDemo.java
@@ -0,0 +1,23 @@
+package lang;
+
+public class StringBuilderDemo {
+
+    public static void main(String[] args) {
+        String[] words = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]);
+            if (i < words.length - 1) {
+                sb.append(", ");
+            }
+        }
+        System.out.println(sb.toString());
+
+        StringBuilder reverse = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reverse.append(words[i]);
+        }
+        System.out.println(reverse.toString());
+    }
+}
